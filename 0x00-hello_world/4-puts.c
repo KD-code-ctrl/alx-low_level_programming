1a3247616addcd5a5737585a84d476cb93e3583b
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-20 09:25:37 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzel,followed by a new line.
- * Return : Always 0
+ *main - Prints Programming is like building a multilingual puzzel
+ *followed by a new line.
+ *Return : Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzel");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }	
