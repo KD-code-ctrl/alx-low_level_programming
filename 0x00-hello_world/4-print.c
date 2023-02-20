b6c90da913cc2b5dce3fef4c11f8b2bf7fcf3133
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-20 08:44:08 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-print.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *
+ * main- 'prints Programming is like building a multilingual puzzle'
+ *
+ * Return always 0.
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzel");
+
+	return (0);
+}
+	
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,2 @@
-#include <studio.h>
-/**
- * main entry point
- * 
- * should print 'Programming is like building a multilingual puzzle'
- *
- * Return: Always 0
- * 
- */
-
-
-int main(void)
-{
-
-	char string1[] = "Programming is like building a multilingual puzzle";
-
-	puts(string1);
-
-	return 0;
-
-}
-
+#!/bin/bash
+gcc 4-print.c -o 4-puts.c
