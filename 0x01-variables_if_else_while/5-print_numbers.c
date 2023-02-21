63fa54e2cb43ac2892e78a30b550b1d7d398f6d9
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-21 06:56:54 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print singel digit numbers of base 10
+ * Return:0 when successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	       printf("%d", i);
+	putchar('\n');
+	return (0);
+}
