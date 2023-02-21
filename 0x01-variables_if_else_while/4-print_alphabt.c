658a8a74c35369e7280e9c5938b9e693337d862f
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-21 06:26:29 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print letters of the alphabet except for q & e
+ * Return:0 when successful
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		if (ch == 'q' || ch == 'e')
+			continue;
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
