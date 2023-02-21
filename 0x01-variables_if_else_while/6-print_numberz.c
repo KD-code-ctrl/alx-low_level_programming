daa4b2c0a4535854a86b17d2c5b5c2a9619d04d0
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-21 07:15:05 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print singel digits of base 10 using putchar()
+ * Return:0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i+'0');
+	putchar('\n');
+	return (0);
+}
