fde09f01939957d83da5df99882759643059f3d8
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-22 03:57:42 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-int _putchar(char c);
 
 /**
  * main - print a character from the _putchar function
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,1 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes out a character passed in as argument
- * Return:1 on success
- */
-int _putchar(char c)
-{
-	return (write(1 , &c , 1));
-}
+int _putchar(char c);
