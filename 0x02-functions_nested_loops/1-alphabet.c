5b50196447bb52bfa6f4afddfd34942268e3fba0
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-22 05:02:00 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the letters aof the alphabet in lower case
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
