ab82c997fbf1eac02e07c22ad09eac54d207ed37
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-22 07:49:20 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - checks if the letter is lower case
+ * Return:0 if its true
+ */
+int main(void)
+{
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
