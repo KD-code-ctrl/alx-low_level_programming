988a87229410a6083a3b9b152342d040cd815567
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-22 13:28:48 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - print the absolute value of intger;
+ * @int: is the number to be checked
+ * Return:0 if successful
+ */
+int _abs(int a)
+{
+	_putchar(a);
+	return (0);
+}
