c181c54de578859ceace9553e198a6575d27a34f
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-21 23:45:42 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,20 @@
+#include <main.h>
+#include <string.h>
 
+/**
+ * main - print a character from the _putchar function
+ * Return:0 if successful
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
