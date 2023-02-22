b13f03ec9908b065f4392228a0e7c429a59af168
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-22 15:52:01 -0800

    9-times_table

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - has function time_table
+ * Return:0 always
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * times_table - prints out the multi table 
+ * Return:0 when successful
+ */
+void times_table(void)
+{
+	int i, x;
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			int result = i * x;
+			_putchar(result + '0');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
