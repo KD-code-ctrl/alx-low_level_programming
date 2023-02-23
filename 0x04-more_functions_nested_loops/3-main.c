9e949d8efd90dd1e9b316f750cc2f18840b226ee
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-23 12:29:14 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - has  a function that print numbers upto a range
+ * Return:0 always
+ */
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number - prints numbers up to a range
+ * Return:0 when successful
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
