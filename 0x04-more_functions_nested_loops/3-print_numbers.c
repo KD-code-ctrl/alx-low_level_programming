1ca7a39b65c930805cb5ffdf29f55dd786a362e6
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-23 12:33:34 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_number - prints numbers up to a range
+ * print_numbers - prints numbers up to a range
  * Return:0 when successful
  */
 void print_numbers(void)
@@ -9,6 +9,6 @@ void print_numbers(void)
 	int i;
 
 	for (i = 0 ; i <= 9 ; i++)
-		_putchar(i);
+		_putchar((i % 10) + '0');
 	_putchar('\n');
 }
