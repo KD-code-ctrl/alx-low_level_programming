11762d4ada2830eba6d1c63b3607a7c38de269e2
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-23 12:07:30 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main has the funtion that multiplys two numbers
+ * Return:0 always
+ */
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplys two integers
+ * @a: is argument passed in.
+ * @b: is argument passed in.
+ * Return: Result of the two integers
+ */
+int mul(int a, int b)
+{
+	int result;
+	result = a * b;
+	return (result);
+}
