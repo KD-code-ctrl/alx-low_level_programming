5713df514f7ae4d1811ad2145b9c6048e9c228ad
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-22 16:27:42 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,16 +6,19 @@
  */
 void times_table(void)
 {
-	int i, x;
+	int i, x,result;
 	for (i = 0 ; i <= 9 ; i++)
 	{
+		_putchar('0');
 		for (x = 0; x <= 9; x++)
 		{
-			int result = i * x;
-			_putchar(result);
+			result = i * x;
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
+			if (result > 9)
+				_putchar((result/10) + '0');
+			_putchar((result % 10) + '0');
 		}
 		_putchar('\n');
 	}
