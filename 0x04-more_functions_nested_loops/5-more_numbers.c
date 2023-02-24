f0552a10afc6473e519e76ab64eb64ca53ffe7d8
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-24 07:13:42 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,10 +13,13 @@ void more_numbers(void)
 	{
 		for (x = 0 ; x <= 14 ; x++)
 		{
+			if (x > 9)
+			{
+				_putchar((x / 10) + '0');
+			}
 			_putchar((x % 10) + '0');
 		}
 		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
 }
