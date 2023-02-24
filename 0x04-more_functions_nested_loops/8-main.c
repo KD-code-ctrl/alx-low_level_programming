2c75cadbe1437cad55ede901021a9ebb38761482
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-24 14:03:44 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - has a function that prints a square
+ * Return:0 always
+ */
+int main(void)
+{
+	print_square(2);
+	print_square(10);
+	print_square(0);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: is the size of the square
+ * Return:0 when successful
+ */
+void print_square(int size)
+{
+	int x,i;
+
+	if (size > 0)
+	{
+		for (x = 0 ; x < size ; x++)
+		{
+			for (i = 0 ; i < size ; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
