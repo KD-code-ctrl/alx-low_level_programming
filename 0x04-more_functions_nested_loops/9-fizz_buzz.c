6d8e452dbe1f113964516e0a1e88f17ec05a1b46
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-24 17:05:26 -0800

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 1-100 if num is multiple of 3 prints fizz
+ * 	if num is multiple of 5 print buzz if num num multiple of
+ * 	both 5&3 prints fizzbuzz or else prints num
+ * Return:0 if successful
+ */
+int main()
+{
+	int n;
+
+	n = 1;
+	while (n < 100 + 1)
+	{
+		if(n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if(n % 3 == 0)
+		{
+			printf("fizz ");
+		}
+		else if(n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		n++;
+	}
+	return (0);
+}
