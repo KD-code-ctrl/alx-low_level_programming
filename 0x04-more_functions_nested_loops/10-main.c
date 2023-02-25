8e5078a00f5912980939a222d78a0688a30fa555
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-24 19:49:14 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - has a function that prints a triangel
+ *
+ * Return:0 always
+ */
+int main(void)
+{
+	print_triangle(2);
+	print_triangle(10);
+	print_triangle(1);
+	print_triangle(0);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangel - prints a shape of a triangel
+ * @size: determines how big the triangel should be
+ * Return:0 if successful
+ */
+void print_triangle(int size)
+{
+	int i,x,space;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size ; x++)
+		{
+			for (space = size - x ; space > 1 ; space--)
+			{
+				_putchar(' ');
+			}
+
+			for (i = 0 ; i <= x ; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
