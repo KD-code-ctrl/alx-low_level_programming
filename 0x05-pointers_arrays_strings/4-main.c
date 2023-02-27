4077963fe1b615df0cff6d1c4a0cf108707a0cb7
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-27 14:16:39 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - has function print_rev
+ * Return:0 when successful
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
