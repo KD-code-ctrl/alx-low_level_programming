b85f4e5aee476b3367ed5171d4b042287d4b910f
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-27 05:30:22 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - is a function that takes int as parameter and updates the value
+ * reset_to_98 - takes int as parameter and updates it
  * @x: is a pointer variable
  * Return:0  when successful
  */
