a18034dea86e3a92d1ffc8cf97b435bb2118ac79
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-02-27 06:14:58 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * swap_int - swaps to integers around 
- * @a: is pointer variable 
+ * swap_int - swaps to integers around
+ * @a: is pointer variable
  * @b: is a pointer variable
  * Return:0  when successful
  */
-void swap_int(int *a,int *b)
+void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
 }
