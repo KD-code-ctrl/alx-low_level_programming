db673a5e2a906dc9f0a4fc632d13a25c7873520b
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-03 09:34:46 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,34 +2,27 @@
 
 /**
  * cap_string - is a function that capitalizes evry word in the string
- * @string: is a pointer variable to a string
+ * @str: is a pointer variable to a string
  * Return: the capitalized string
  */
-char *cap_string(char *string)
+char *cap_string(char *str)
 {
-	int i;
+	int i = 0;
 
-	while (string[i] != '\0')
+	while (str[i])
 	{
-		if (i == 0 || string[i - 1] == ' ' || string[i - 1] == '\t' ||
-		string[i - 1] == '\n' || string[i - 1] == ',' || string[i - 1] == ';' ||
-		string[i - 1] == '.' || string[i - 1] == '!' || string[i - 1] == '?' ||
-		string[i - 1] == '"' || string[i - 1] == '(' || string[i - 1] == ')' ||
-		string[i - 1] == '{' || string[i - 1] == '}')
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
+		str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' ||
+		str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||
+		str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
+		str[i - 1] == '{' || str[i - 1] == '}')
 		{
-			if (string[i] >= 'a' && string[i] <= 'z')
-			{
-				string[i] = string[i] - 32;
-			}
+			str[i] -= 32;
 		}
-		else
-		{
-			if (string[i] >= 'A' && string[i] <= 'Z')
-			{
-				string[i] = string[i] + 32;
-			}
-		}
+
 		i++;
 	}
-	return (string);
+	return (str);
 }
