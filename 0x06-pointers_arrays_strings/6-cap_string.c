5b448c1c8f78bfa1861b73c78de74631994c96c5
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-03 09:07:45 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,48 +2,34 @@
 
 /**
  * cap_string - is a function that capitalizes evry word in the string
- * @str: is a pointer variable to a string
+ * @string: is a pointer variable to a string
  * Return: the capitalized string
  */
-char *cap_string(char *str)
+char *cap_string(char *string)
 {
 	int i;
 
-	for (i = 1; str[i] != '\0'; i++)
+	while (string[i] != '\0')
 	{
-		if (str[1] <= 'a' && str[1] >= 'z')
+		if (i == 0 || string[i - 1] == ' ' || string[i - 1] == '\t' ||
+		string[i - 1] == '\n' || string[i - 1] == ',' || string[i - 1] == ';' ||
+		string[i - 1] == '.' || string[i - 1] == '!' || string[i - 1] == '?' ||
+		string[i - 1] == '"' || string[i - 1] == '(' || string[i - 1] == ')' ||
+		string[i - 1] == '{' || string[i - 1] == '}')
 		{
-			str[1] = str[1] - 32;
-		}
-		if (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')))
-		{
-			i++;
-			if (str[i] == 32 || str[i] == '\t' || str[i] == '\n' || str[i] == ';'
-			|| str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' ||
-			str[i] == ')' || str[i] == '{' || str[i] == '}' || str[i] == 46 ||
-			str[i] == 44)
+			if (string[i] >= 'a' && string[i] <= 'z')
 			{
-				i++;
-				if (str[i] >= 'a' && str[i] <= 'z')
-				{
-					str[i] = str[i] - 32;
-				}
-			}
-			else
-			{
-				if (str[i] >= 'a' && str[i] <= 'z')
-				{
-					str[i] = str[i] - 32;
-				}
+				string[i] = string[i] - 32;
 			}
 		}
 		else
 		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
+			if (string[i] >= 'A' && string[i] <= 'Z')
 			{
-				str[i] = str[i] + 32;
+				string[i] = string[i] + 32;
 			}
 		}
+		i++;
 	}
-	return (str);
+	return (string);
 }
