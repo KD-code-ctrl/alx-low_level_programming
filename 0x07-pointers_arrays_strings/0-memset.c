eb4002fdf7b06a015fe310d26f38784b8aeceefb
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-06 12:21:25 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _memset - function fills the first n bytes of the memory area pointed to by s
- *  with the constant byte b
+ * _memset - function fills the first n bytes of the memory area pointed to by
+ * s with the constant byte b
  *  @n: is number of blocks to be replaced
  *  @b: is the charcter to be inserted
  *  @s: is the pointer to the string
@@ -10,9 +10,9 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	 int i;
 
-	for (i = 0 ; i <= n ; i++)
+	for (i = 0 ; i < n ; i++)
 	{
 		s[i] = b;
 	}
