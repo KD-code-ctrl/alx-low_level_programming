befaf1b844f9a8f4ca6749f5ef2ff822a67a1c96
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-06 11:51:18 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * memset - function fills the first n bytes of the memory area pointed to by s
+ *  with the constant byte b
+ *  @n: is number of blocks to be replaced
+ *  @b: is the charcter to be inserted
+ *  @s: is the pointer to the string
+ *  Return: s modified
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i <= n ; i++)
+	{
+		if (s[i] != ' ')
+		{
+			s[i] = b;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return (s);
+}
