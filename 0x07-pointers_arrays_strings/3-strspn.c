80184b005792957abfe01b7673e6606569ef7f69
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-07 06:56:39 -0800

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strspn - is a function that gets the length of a prefix substring
+ * @s: pointer to the string
+ * @accept: is the string to be searched for
+ * Return: n bytes of accept found in s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, x, len, max, total;
+
+	max = 0;
+	total = 0;
+	len = 0;
+
+	while (s[max])
+	{
+		max++;
+	}
+	while (accept[total])
+	{
+		total++;
+	}
+	for (i = 0 ; i <= total ; i++)
+	{
+		for (x = 0 ;  x <= max ; x++)
+		{
+			if (accept[i] == s[x])
+			{
+				len++;
+				break;
+			}
+		}
+	}
+	return (len);
+}
