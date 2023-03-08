96788118f4d2d2721722f9089adcc59ea33458e3
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-08 06:38:13 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_chessboard(char (*a)[8])
- * @(*a)[8]: is the array we will be looping through
+ * print_chessboard - is a function that prints chessboard
+ * @a: is the pointer to row
  * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0 ; i < 8 ; i++)
+	for (i = 0 ; i <= 8 ; i++)
 	{
-		for (j = 0 ; j < 8 ; j++)
+		for (j = 0 ; j <= 8 ; j++)
 		{
 			_putchar(a[i][j]);
 			if (a[i][j] == ' ')
