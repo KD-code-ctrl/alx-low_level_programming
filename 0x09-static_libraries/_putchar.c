a3aa475f0ffcaa90e7a9d4d360a9f368ba02cd0c
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-10 09:27:03 -0800

    all

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,12 +1,10 @@
 #include "main.h"
 #include <unistd.h>
 
-/**
- * _putchar - printts out the latter passed as an argument
- * @c: is the charcter argument
+/** putchar - printts out the latter passed as an argument
  * Return:1 on success
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return (write(1 , &c , 1));
 }
