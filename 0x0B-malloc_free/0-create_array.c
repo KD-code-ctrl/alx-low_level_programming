f4d0f93f84ac8fa66c27945d3fd23bef0ddcfc3c
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-14 01:26:22 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - is a function that creates an array of chars
+ * and initializes it with a specific char
+ * @size: size is an integer value that will determine the
+ * size of the array
+ * @c: is a character
+ * Return: a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr;
+
+	if (size == 0)
+	{
+		return ('\0');
+	}
+	arr = malloc(size * 1);
+	arr[0] = c;
+	for (i = 1 ; i < size ; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+What is the difference between automatic and dynamic allocation
+What is malloc and free and how to use them
+when to  use malloc
+
