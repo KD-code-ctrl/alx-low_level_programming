677524b022b7dfd5d1e6741b4ec923bc46668c39
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-15 09:32:40 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,16 +12,19 @@ int main(int argc, char *argv[])
 {
 	int i, result;
 
-	if (argc == 1)
+	if (argc >= 3)
 	{
-		printf("Error");
-		return (1);
-	}
-	result = 1;
-	for (i = 1 ; i < argc ; i++)
-	{
-		result = result * atoi(argv[i]);
+		result = 1;
+		for (i = 1 ; i < argc ; i++)
+		{
+			result = result * atoi(argv[i]);
+		}
 		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
