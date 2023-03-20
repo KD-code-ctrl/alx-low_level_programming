62a95820f48fd3f4c1387be8d785dc71aed066b2
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-20 13:37:48 -0700

    Created a new directory with file README.md and dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+This directory is all about structures and typedef
+The outcome should be that one can answer these questions
+What are structures, when, why and how to use them
+How to use typedef.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is a structure with multiple elements
+ * @name: is the name of the dog
+ * @age: is the dods age
+ * @owner: is the owners name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
