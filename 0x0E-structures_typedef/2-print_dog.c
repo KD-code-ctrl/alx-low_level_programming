876662c18ede973621b9663e63627085685bc5ec
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-20 15:26:15 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,31 +9,33 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d == NULL)
 	{
-		if (d->name == NULL)
-		{
-			printf("Name: (nil)\n");
-		}
-		else
-		{
-			printf("Name: %s\n", d->name);
-		}
-		if (d->age < 0)
-		{
-			printf("Age: %d\n", 0);
-		}
-		else
-		{
-			printf("Age: %.1f\n", d->age);
-		}
-		if (d->owner == NULL)
-		{
-			printf("Owner: (nil)\n");
-		}
-		else
-		{
-			printf("Owner: %s\n", d->owner);
-		}
+		return;
 	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	if (d->age < 0)
+	{
+		printf("Age: %d\n", 0);
+	}
+	else
+	{
+		printf("Age: %.1f\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	
 }
