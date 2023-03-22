3e25e9aa900594a27a0131de231d48be60324f30
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-22 11:56:09 -0700

    Put in some check statement

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,6 +15,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i, hold, track;
 
 	track = 0;
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
 	if (size <= 0)
 	{
 		return (-1);
