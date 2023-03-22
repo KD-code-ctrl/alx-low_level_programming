febbaf80891914d748b35c9e793d00b9f657587a
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-22 07:16:06 -0700

    Created file 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - is a function that prints a name
+ * @name: is a  pointer to a character string
+ * @f: is a pointer to a function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+}
