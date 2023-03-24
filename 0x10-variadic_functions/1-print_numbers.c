f759f20e27bb59caa0b32c8378acbca156f906e5
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-03-24 06:30:46 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,9 +19,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		num = va_arg(args, int);
 		printf("%d", num);
-		if (i != n - 1 && separator != NULL)
+		if ((i != n - 1) && (separator != NULL))
 		{
-			printf("%s ", separator);
+			printf("%s", separator);
 		}
 	}
 	va_end(args);
