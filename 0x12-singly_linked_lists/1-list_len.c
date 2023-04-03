9b95b53ab04d939a4ba81e723cdf713cc85e233c
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-04-03 11:04:51 -0700

    Created file 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <string.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * list_len - function that returns the number of elements in a
+ * linked list
+ * @h: is a pointer to a structure
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned num_el = 0;
+
+	while (h != NULL)
+	{
+		num_el++;
+		h = h->next;
+	}
+	return (num_el);
+}
