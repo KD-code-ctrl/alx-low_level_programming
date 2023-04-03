c68e8c4f1dfb5d9b78e7ad7b2f33ee9fd6fb9fd8
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-04-03 15:18:47 -0700

    Fixed some betty styling issues

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,12 +7,13 @@
 /**
  * add_node - function that add a new node at the biginning of a list
  * @head: is a pointer to a pointer of  type 'list_t'
- * @str: is a pointer to string 
+ * @str: is a pointer to string
  * Return: address of the new element
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp = malloc(sizeof(list_t));
+
 	if (temp == NULL)
 	{
 		return (NULL);
