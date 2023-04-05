fad08e466240eb6476029ddb7993ea0349103398
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-04-05 15:22:18 -0700

    Created file 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * pop_listint - is a function that deletes the head node of a list
+ * @head: is a pointer to a struct
+ * Return: heads node data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int i;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	if (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		i = temp->n;
+		free(temp);
+	}
+	return (i);
+}
