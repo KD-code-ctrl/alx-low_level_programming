7107f7a63f187833060aa672794fb499e5ae16ff
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-04-06 12:32:36 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		free(temp);
 		return (NULL);
 	}
-	if (idx == 1)
+	if (idx == 0)
 	{
 		temp->next = *head;
 		*head = temp;
