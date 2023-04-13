f357bfe25558cfade18a2e6c85d3e1151599bdbe
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-04-13 01:39:44 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,26 +12,17 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i, len, sum;
 
-	len = 0;
-	i = 0;
-	sum = 0;
 	len = _strlen(b);
 	if (len == 0)
 	{
 		return (0);
 	}
-	if (*b == '\0')
-	{
-		return (0);
-	}
+	sum  = 0;
 	for (i = 0; i < len ; i++)
 	{
 		if (b[i] == '0' || b[i] == '1')
 		{
-			if (b[i] == '1')
-			{
-				sum += (b[i] - '0') * _pow(2, len - i - 1);
-			}
+			sum = (sum << 1) + (b[i] - '0');
 		}
 		else
 		{
@@ -40,30 +31,6 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (sum);
 }
-
-/**
- * _pow - is a function that return a power of a number
- * @base: is the the base
- * @exponent: is the exponent
- * Return: the result of base to the power of exponent
- */
-int _pow(int base, int exponent)
-{
-	int result = 1;
-	int i;
-
-	if (exponent < 0)
-	{
-		base = 1 / base;
-		exponent = -exponent;
-	}
-	for (i = 0; i < exponent; i++)
-	{
-		result *= base;
-	}
-	return (result);
-}
-
 /**
  * _strlen -  is a function that returns a strings length
  * @s: is a pointer to string
