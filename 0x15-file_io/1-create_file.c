b67128d52ff580cec8810d68bebf0f6867edb9cd
KD-code-ctrl <katlehodlamini.kd@outlook.com>
2023-04-17 01:40:21 -0700

    Fixed some style issues

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fp = open(filename,O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fp < 0)
 	{
 		return (-1);
@@ -36,6 +36,6 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	close (fp);
+	close(fp);
 	return (1);
 }
